0c02f7ae83057cba4f97dbd76368927b200c4d0e
Cedric Willems <willems_cedric@hotmail.com>
2017-08-11 01:30:04 +0200

    'full'

--- index.js
+++ index.js
@@ -7,20 +7,29 @@ var app = express();
 var request = require("request")
 
 var crypto_url = [
-					"https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR",
-					"https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=LTC,USD,EUR",
-					"https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=DOGE,USD,EUR"
+					"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=BTC,USD,EUR",
+					"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=LTC,USD,EUR",
+					"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=DOGE,USD,EUR",
+					"https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=ETH,USD,EUR",
+					"https://www.cryptopia.co.nz/api/GetMarket/DCN_BTC",
+					"https://www.cryptopia.co.nz/api/GetMarket/DCN_LTC",
+					"https://www.cryptopia.co.nz/api/GetMarket/DCN_DOGE"
 				];
-var crypto_currency = [];
+
+var DCN = 800000;
+var BTC;
+var LTC;
+var DOGE;
+var ETH;
+var DCN_BTC;
+var DCN_LTC;
+var DCN_DOGE;
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-
-var url = "http://developer.cumtd.com/api/v2.2/json/GetStop?" +
-    "key=d99803c970a04223998cabd90a741633" +
-    "&stop_id=it"
 
 crypto_url.map(function (item){
 	request({
@@ -29,19 +38,45 @@ crypto_url.map(function (item){
 		}, function (error, response, body) {
 
 	    	if (!error && response.statusCode === 200) {
-	    	console.log(body);
-	        crypto_currency.push(body);
-	    	}
+
+	    		//console.log(body);
+
+				if(body["BTC"] !== undefined){
+			    	BTC = body;
+					console.log("BTC");
+				}else if(body["LTC"] !== undefined){
+			    	LTC = body;
+	    			console.log("LTC");
+				}else if(body["DOGE"] !== undefined){
+			    	DOGE = body;
+	    			console.log("DOGE");
+				}else if(body["ETH"] !== undefined){
+			    	ETH = body;
+	    			console.log("ETH");
+
+				}else if(body.Data.Label === "DCN/BTC"){
+			    	DCN_BTC = body.Data;
+	    			console.log("DCN_BTC");
+				}else if(body.Data.Label === "DCN/LTC"){
+			    	DCN_LTC = body.Data;
+	    			console.log("DCN_LTC");
+				}else if(body.Data.Label === "DCN/DOGE"){
+			    	DCN_DOGE = body.Data;
+	    			console.log("DCN_DOGE");
+				}							
+
+    		}
 		})
 	
 });		
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index', {BTC: crypto_currency[0], LTC: crypto_currency[1], DOGE: crypto_currency[2], name: 'Cedric'});
+  response.render('pages/index', {DCN: DCN, BTC: BTC, LTC: LTC, DOGE: DOGE, ETH: ETH, DCN_BTC: DCN_BTC, DCN_LTC: DCN_LTC, DCN_DOGE: DCN_DOGE});
 });
 
 app.get('/cool', function(request, response) {
